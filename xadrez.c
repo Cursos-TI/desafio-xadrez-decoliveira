ef4b89adaadad1b5fc3356bc155b456cfdeb1ba5
decoliveira <ao79373@gmail.com>
2025-10-15 16:26:26 -0300

    Atualização de código - Desafio Xadrez - Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
 
-int main() {
-    int i = 0;
+void moverTorre(int casas) {
+    if (casas == 0) return;
+    printf("Direita\n");
+    moverTorre(casas - 1);
+}
 
-    printf("***TORRE***\n");
-    while (i < 5) {
-        printf("Direita\n");
-        i++;
+void moverBispo(int movimento, int atual) {
+    if (atual > movimento) return;
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Cima, Direita\n");
+        }
     }
+    moverBispo(movimento, atual + 1);
+}
 
-    i = 0;
+void moverRainha(int casas) {
+    if (casas == 0) return;
+    printf("Esquerda\n");
+    moverRainha(casas - 1);
+}
+
+void moverCavalo() {
+    printf("***CAVALO***\n");
+    int movVertical = 2;
+    int movHorizontal = 1;
+    for (int i = 1; i <= movVertical; i++) {
+        for (int j = 1; j <= movHorizontal + 1; j++) {
+            if (i < movVertical) {
+                printf("Cima\n");
+                continue;
+            }
+            if (j == movHorizontal) {
+                printf("Direita\n");
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    printf("***TORRE***\n");
+    moverTorre(5);
+
     printf("\n\n***BISPO***\n");
-    do
-    {
-        printf("Cima, Direita\n");
-        i++;
-    } while (i < 5);
-    
+    moverBispo(5, 1);
 
     printf("\n\n***RAINHA***\n");
-    for (i = 0; i < 8; i++)
-    {
-        printf("Esquerda\n");
-    }
+    moverRainha(8);
 
-    int j = 1;
-    printf("\n\n***CAVALO***\n");
-    for (i = 1; i <= 1; i++)
-    {
-        while (j <= 2)
-        {
-            printf("Baixo\n");
-            ++j;
-        }
-    
-        printf("Esquerda\n");    
-    }
-    
+    printf("\n\n");
+    moverCavalo();
+
     return 0;
 }
